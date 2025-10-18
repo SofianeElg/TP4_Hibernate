91fbe7dd9827b126cd42d1515d851c90a81edea9
Sofian <sofiane02.elghazzali@gmail.com>
2025-10-18 11:26:11 +0100

    first commit

---
+++ src/main/java/entities/Machine.java
@@ -0,0 +1,82 @@
+package entities;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@NamedNativeQuery(
+        name = "findBetweenDateNative",
+        query = "SELECT * FROM machine WHERE dateAchat BETWEEN :d1 AND :d2",
+        resultClass = Machine.class
+)
+@NamedQuery(
+        name = "findBetweenDate",
+        query = "FROM Machine WHERE dateAchat BETWEEN :d1 AND :d2"
+)
+public class Machine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String ref;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateAchat;
+
+    @ManyToOne
+    private Salle salle;
+
+    // --- Constructeurs ---
+
+    public Machine() {
+        // Constructeur par défaut requis par JPA
+    }
+
+    public Machine(String ref, Date dateAchat, Salle salle) {
+        this.ref = ref;
+        this.dateAchat = dateAchat;
+        this.salle = salle;
+    }
+
+    public Machine(int id, String ref, Date dateAchat, Salle salle) {
+        this.id = id;
+        this.ref = ref;
+        this.dateAchat = dateAchat;
+        this.salle = salle;
+    }
+
+    // --- Getters & Setters ---
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public Date getDateAchat() {
+        return dateAchat;
+    }
+
+    public void setDateAchat(Date dateAchat) {
+        this.dateAchat = dateAchat;
+    }
+
+    public Salle getSalle() {
+        return salle;
+    }
+
+    public void setSalle(Salle salle) {
+        this.salle = salle;
+    }
+}
